0297414f878b4de5972c9e17c200b5437bac2254
Nguyễn Viết Công <91941271+nvcong1710@users.noreply.github.com>
2024-11-01 16:45:12 +0700

    merge main

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,5 @@
 import { BookSlider, AuthorSlider } from "../../component/Slider";
-import axios from "axios";
-import React, { useEffect, useState, useContext } from "react";
+import React, { useState, useContext } from "react";
 import { UserContext } from "../../../src/context/UserContext";
 function HomePage() {
   const [authors, setAuthors] = useState();
@@ -8,16 +7,13 @@ function HomePage() {
   const { user } = useContext(UserContext);
 
   return (
-    <div className="bg-white bg-blue-50">
+    <div className="bg-white">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 flex">
         <div className="">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             <span className="block">Sách mới - Mang trí thức đến mọi nơi</span>
           </h2>
           <div className="mt-8">
-            {/* <button className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              Mua sắm ngay
-            </button>/ */}
           </div>
         </div >
       </div >
